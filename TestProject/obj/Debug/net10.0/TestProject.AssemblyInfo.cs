af18d7aead4eff4dcbb6327e9da55b355769cadc
cedric <cedricnzia@gmail.com>
2026-06-28 15:49:48 +0200

    Fix

--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82d4cf43004c35c99248c6ca6eb92df3eb2b671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+331df1f0264f779156fab5b6a451188aaca9e7b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
